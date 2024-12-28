56e8754aabb75398b055fcbf673f1f8db5e96368
wcy-2280 <2280374571@qq.com>
2024-12-28 18:54:35 +0800

    Update sky.py

--- code/sky.py
+++ code/sky.py
@@ -15,7 +15,7 @@ class Sky:
     def display(self,dt):
         for index, value in enumerate(self.end_color):
             if self.start_color[index] > value: # 变暗
-                self.start_color[index] -= 10 *dt
+                self.start_color[index] -= 2 *dt
 
         self.full_surf.fill(self.start_color)
         self.display_suface.blit(self.full_surf, (0,0), special_flags = pygame.BLEND_RGBA_MULT)
